ff8c41af55b361d843e986d415dbd8e27e6d8d34
Guilherme Anjos <49658386+Guilherme-dev15@users.noreply.github.com>
2023-07-26 11:01:22 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,52 +1,68 @@
+// JavaScript
+let characterLow = "abcdefghijklmnopqrstuvwxyz";
+let characterUpper = characterLow.toUpperCase();
+let specialCharacters = "!@#$%^&*()_-+*/";
+let characterNumbers = "0123456789";
 let character = "";
-const characterLow = "abcdefghijklmnopqrstuvwxyz";
-const characterUpper = characterLow.toUpperCase();
-const specialCharacters = "!@#$%^&*()_-+*/";
-const characterNumbers = "0123456789";
 
 const lowCheckbox = document.getElementById("chkCharacterLow");
 const upperCheckbox = document.getElementById("chkCharacterUpper");
 const numberCheckbox = document.getElementById("chkCharacterNumbers");
 const specialCheckbox = document.getElementById("chkSpecialCharacters");
+
+lowCheckbox.addEventListener("change", updateCharacter);
+upperCheckbox.addEventListener("change", updateCharacter);
+numberCheckbox.addEventListener("change", updateCharacter);
+specialCheckbox.addEventListener("change", updateCharacter);
+
 const btnGenerate = document.getElementById("btn-generate");
 const btnDelete = document.getElementById("btn-delete");
 const btnCopy = document.getElementById("btn-copy");
+const passwordInput = document.getElementById("password");
 
 btnGenerate.addEventListener("click", generatePassword);
 btnDelete.addEventListener("click", deletePassword);
 btnCopy.addEventListener("click", copyText);
 
-function generatePassword() {
-  const pwdLength = document.getElementById("lengthPassword").value;
+function updateCharacter() {
+  character = '';
+  
   if (lowCheckbox.checked) {
     character += characterLow;
   }
+
   if (upperCheckbox.checked) {
     character += characterUpper;
   }
+
   if (numberCheckbox.checked) {
     character += characterNumbers;
   }
+
   if (specialCheckbox.checked) {
     character += specialCharacters;
   }
+}
 
-  let password = '';
+function generatePassword() {
+  const pwdLength = document.getElementById("lengthPassword").value;
+  updateCharacter(); 
+
+  let password = "";
   for (let i = 0; i < pwdLength; i++) {
     let position = Math.floor(Math.random() * character.length);
     password += character.charAt(position);
   }
 
-  document.getElementById("password").value = password;
+  passwordInput.value = password;
 }
 
 function deletePassword() {
-  document.getElementById("password").value = "";
+  passwordInput.value = "";
 }
 
 function copyText() {
-  let textElement = document.getElementById("password");
-  textElement.select();
-  textElement.setSelectionRange(0, 99999);
+  passwordInput.select();
+  passwordInput.setSelectionRange(0, 99999);
   document.execCommand("copy");
 }
